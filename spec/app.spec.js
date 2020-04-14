74363d1a7cd66be7c3448d2a8ef810dacf902fbc
nax2uk <azlinayeo@gmail.com>
2020-04-14 16:06:48 +0100

    created app.js, app.spec.js with required describe blocks for http requests

---
+++ spec/app.spec.js
@@ -0,0 +1,51 @@
+process.env.NODE_ENV = "test";
+const { expect } = require("chai");
+const request = require("supertest");
+const app = require("../app");
+
+describe("#app", () => {
+  describe("#/api", () => {
+    // **GET** `/api`-
+    describe("#GET", () => {});
+
+    describe("#/topics", () => {
+      // **GET** `/api/topics`-
+      describe("#GET");
+    });
+
+    describe("#/users", () => {
+      // **GET** `api/users`
+      describe("#GET", () => {});
+    });
+
+    describe("#/articles", () => {
+      // **GET** `api/articles`
+      describe("#GET", () => {});
+
+      describe("/:article_id", () => {
+        // **GET** `api/articles/:article_id
+        describe("#GET", () => {});
+
+        // **PATCH** `api/articles/:article_id
+        describe("#PATCH", () => {});
+
+        describe("#/comments", () => {
+          // **GET** `api/articles/:article_id/comments
+          describe("#GET", () => {});
+
+          // **POST** `api/articles/:article_id/comments
+          describe("#POST", () => {});
+        });
+      });
+    });
+
+    describe("#/comments", () => {
+      describe("#/:comment_id", () => {
+        // **PATCH** `/api/comments/:comment_id`
+        describe("#PATCH", () => {});
+        // **DELETE** `/api/comments/:comment_id`
+        describe("#DELETE", () => {});
+      });
+    });
+  });
+});
