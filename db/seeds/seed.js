07419168b16b01b6187a01773a9922508ce46428
nax2uk <azlinayeo@gmail.com>
2020-04-14 11:25:00 +0100

    added index.js files to db/data/development-data, db/data/test-data and db/

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,14 +2,14 @@ const {
   topicData,
   articleData,
   commentData,
-  userData
-} = require('../data/index.js');
+  userData,
+} = require("../data/index.js");
 
-const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
+const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
-exports.seed = function(knex) {
-  const topicsInsertions = knex('topics').insert(topicData);
-  const usersInsertions = knex('users').insert(userData);
+exports.seed = function (knex) {
+  const topicsInsertions = knex("topics").insert(topicData);
+  const usersInsertions = knex("users").insert(userData);
 
   return Promise.all([topicsInsertions, usersInsertions])
     .then(() => {
@@ -17,12 +17,12 @@ exports.seed = function(knex) {
       
       Your article data is currently in the incorrect format and will violate your SQL schema. 
       
-      You will need to write and test the provided formatDate utility function to be able insert your article data.
+      You will neegitd to write and test the provided formatDate utility function to be able insert your article data.
 
       Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
       */
     })
-    .then(articleRows => {
+    .then((articleRows) => {
       /* 
 
       Your comment data is currently in the incorrect format and will violate your SQL schema. 
@@ -34,6 +34,6 @@ exports.seed = function(knex) {
 
       const articleRef = makeRefObj(articleRows);
       const formattedComments = formatComments(commentData, articleRef);
-      return knex('comments').insert(formattedComments);
+      return knex("comments").insert(formattedComments);
     });
 };
