1e8c81419a1f7491f52fc627122d2a0a3bbdb761
nax2uk <azlinayeo@gmail.com>
2020-04-15 11:30:57 +0100

    ALso added the code for GET /api/users/:username!

--- controller/users.controllers.js
+++ controller/users.controllers.js
@@ -1,6 +1,5 @@
 const { fetchUser } = require("../model");
 const getUser = (req, resp, next) => {
-  console.log(req.params.userName);
   fetchUser(req.params.userName).then((objUser) => {
     resp.status(200).send({ user: objUser });
   });
